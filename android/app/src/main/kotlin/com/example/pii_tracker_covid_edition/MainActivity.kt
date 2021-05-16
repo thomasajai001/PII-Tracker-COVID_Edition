4f0c064e8bee2346ff61a49a8f669a60bf5badbb
Thomas Ajai <thomasajaim@outlook.com>
2021-05-16 12:18:33 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/example/pii_tracker_covid_edition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pii_tracker_covid_edition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "PII Tracker - COVID Edition",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PII Tracker"),
+      ),
+    );
+  }
+}
